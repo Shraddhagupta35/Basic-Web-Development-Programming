b5cc16bfbd77f44961f5ce8a1a300b8243a22004
shraddha gupta <132150329+Shraddhagupta35@users.noreply.github.com>
2024-03-12 20:30:55 +0530

    Html & CSS files

---
+++ 3.CSS/5.JAVASCRIPT/1.app.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 20;
+
+// let output = "The total output is : " + (a+b) + "/sec.";
+// console.log(output);
+                   
+                //  using literal method
+
+console.log(`the total output is : ${a+b} /sec.`);                
---
+++ 3.CSS/5.JAVASCRIPT/3.practice.js
@@ -0,0 +1,22 @@
+
+let letter = 'apple';
+
+// if(letter[0] === 'a'){
+// 
+    // console.log("Good String");
+// 
+// }else{
+// 
+    // console.log("Not good string");
+// }
+
+
+if((letter[0] === 'a') && (letter.length > 3)) {
+
+    console.log("Good String");
+
+}else{
+
+    console.log("Not good string");
+
+}
